a379750f02e1807f4b52e4e1acb7cdeb53205568
samyraj42 <45854906+samyraj42@users.noreply.github.com>
2024-12-17 16:16:08 +0530

    Update GuessingGame.cs

--- samyraj/A02/GuessingGame.cs
+++ samyraj/A02/GuessingGame.cs
@@ -1,1 +1,52 @@
 
+namespace A02
+{
+    static internal class GuessingGame
+    {
+        static int noofTry = 1;
+        static bool isSuccess = default(bool);
+        static internal void GuessNumber(int secretnumber)
+        {
+            while (!isSuccess)
+            {
+                Console.WriteLine($"\nNo. {noofTry}");
+                Console.Write("Please guess the number 1-100 : ");
+            
+                if (int.TryParse(Console.ReadLine(),out int num))
+                {
+                    if (num > 0 && num <= 100)
+                    {
+                        switch (num)
+                        {
+                            case var n when num == secretnumber:
+                                isSuccess = true;
+                                Console.WriteLine($"\ncongratulations!.You guessed correctly. No of tries: {noofTry} ");
+                                break;
+
+                            case var n when num < secretnumber:
+                                Console.WriteLine("Your guess is too low.");
+                                break;
+
+                            case var n when num > secretnumber:
+                                Console.WriteLine("Your guess is too high.");
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nPlease enter the number between 1-100. Try again.. ");
+                        noofTry -= 1;
+                    }
+                } 
+                else
+                {
+                    Console.WriteLine("\nInvalid input");
+                    noofTry -= 1;
+                }
+                noofTry++;
+            }
+        }
+
+    }
+}
+
