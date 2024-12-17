3abc76ce992d907a130c7846ab798282000e794f
samyraj42 <45854906+samyraj42@users.noreply.github.com>
2024-12-17 16:29:34 +0530

    Add files via upload

---
+++ samyraj/A02/BinarySearch.cs
@@ -0,0 +1,27 @@
+﻿
+namespace A02
+{
+    internal static class BinarySearch
+    {
+        static int min = 1, max = 100, noofGuess = default(int);
+        static bool isSuccess = default(bool);
+        internal static int Search(int target)
+        {
+            while (!isSuccess) {
+                int mid = MiddleNumber(min, max);
+                if (target == mid) isSuccess = true;
+                else if(target < mid)
+                {
+                    max = mid;
+                }
+                else if(target > mid)
+                {
+                    min = mid;
+                }
+                noofGuess++;
+            }
+            return noofGuess;
+        }
+        static int MiddleNumber(int num1,int num2) => (num1 + num2) / 2;
+    }
+}
---
+++ samyraj/A02/ReverseGuessing.cs
@@ -0,0 +1,38 @@
+﻿
+namespace A02
+{
+    internal class ReverseGuessing
+    {
+        static int min = 1, max = 100, noofGuess = default(int);
+        internal static int Search()
+        {
+            Console.WriteLine("\n Please guess number between 1-100. I am waiting...");
+            Console.ReadLine();
+            while (true)
+            {
+                int mid = MiddleNumber(min, max);
+
+                Console.Write($"\nyour number is {mid} [Y/N]: ");
+
+                var result = Console.ReadKey().Key;
+                if (result == ConsoleKey.Y)
+                {
+                    Console.Write("\n Congratulations!.");
+                    break;
+                }
+                Console.Write($"\n{mid} : is low or high [L/H]: ");
+                if (Console.ReadKey().Key == ConsoleKey.L)
+                {
+                    min = mid;
+                }
+                else
+                {
+                    max = mid;
+                }
+                noofGuess++;
+            }
+            return noofGuess;
+        }
+        static int MiddleNumber(int num1, int num2) => (num1 + num2) / 2;
+    }
+}
